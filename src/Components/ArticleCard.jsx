0308a764e54a9c4852cbb44bc252b2ee0de7e041
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-11 17:48:59 +0000

    add styling to all articles

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,13 +1,13 @@
 const ArticleCard = ({ article }) => {
     return (
         <div className="article-card">
-            <img src={article.article_img_url}></img>
-            <h2 >{article.title}</h2>
-            <p>by {article.author}</p>
-            <p>{article.topic}</p>
+            <p className="article-date">{article.topic} | {article.created_at.slice(0, 10)}</p>
+            <img className="article-card-img" src={article.article_img_url}></img>
+            <h2 className="article-card-title">{article.title} </h2>
+            <p className="article-author">{article.author}</p>
+            {/* <p>{article.topic}</p> */}
             <p>{article.votes} {article.votes === 1 ? "vote" : "votes"}</p>
-            <p>{article.comment_count} {article.comment_count === 1? "comment" : "comments"}</p>
-            <p>{article.created_at.slice(0, 10)}</p>
+            <p>{article.comment_count} {article.comment_count === 1 ? "comment" : "comments"}</p>
         </div>
 
     );
