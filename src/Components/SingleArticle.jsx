1f2f7c41c8c1c3498b723f3e283b3ab132f9c702
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-11 14:50:43 +0000

    add loading

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -3,20 +3,28 @@ import { getSingleArticle } from "../utils/api";
 import { useState } from "react";
 
 const SingleArticle = () => {
+    const [loading, setLoading] = useState(true);
+
     const { article_id } = useParams()
     const [article, setArticle] = useState({});
-    const {article_id: id, article_img_url, author, body, comment_count, created_at, title, topic, votes} = article
+    const { article_id: id, article_img_url, author, body, comment_count, created_at, title, topic, votes } = article
 
     getSingleArticle(article_id)
-        .then(({ article }) => setArticle(article))
+        .then(({ article }) => {
+            setArticle(article)
+            setLoading(false)
+        })
 
     return (
-        <div className="single-article">
-            <h2>{title}</h2>
-            <img id="single-article-img" src={article_img_url}/>
-            <h3>by {author}</h3>
-            <p>{body}</p>
-        </div>
+        <>
+            {loading && <h2>loading...</h2>}
+            <div className="single-article">
+                <h2>{title}</h2>
+                <img id="single-article-img" src={article_img_url} />
+                <h3>by {author}</h3>
+                <p>{body}</p>
+            </div>
+        </>
     );
 }
 
