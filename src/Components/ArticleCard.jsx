79e4d2c071ab4108f3015205cf59b9228bb4fc74
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-11 20:52:51 +0000

    add date conversion

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,14 +1,15 @@
+import convertDate from "../utils/date";
+
 const ArticleCard = ({ article }) => {
     return (
-        <div className="article-card">
-            <p className="article-date">{article.topic} | {article.created_at.slice(0, 10)}</p>
+        <li className="article-card">
+            <p className="article-date">{article.topic} | {convertDate(article.created_at)}</p>
             <img className="article-card-img" src={article.article_img_url}></img>
             <h2 className="article-card-title">{article.title} </h2>
             <p className="article-author">{article.author}</p>
-            {/* <p>{article.topic}</p> */}
             <p>{article.votes} {article.votes === 1 ? "vote" : "votes"}</p>
             <p>{article.comment_count} {article.comment_count === 1 ? "comment" : "comments"}</p>
-        </div>
+        </li>
 
     );
 }
--- src/Components/SingleComment.jsx
+++ src/Components/SingleComment.jsx
@@ -1,9 +1,10 @@
+import convertDate from "../utils/date";
+
 const SingleComment = ({comment}) => {
     const {comment_id, votes, created_at, author, body, article_id} = comment
     return (
         <li className="single-comment">
-            <h3>{author}</h3>
-            <p>{created_at.slice(0, 10)}</p>
+            <p>{author} {convertDate(created_at)}</p>
             <p>{body}</p>
         </li>
     );
