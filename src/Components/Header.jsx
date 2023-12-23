fd08da400e513456e4367e3eecfe64e7b0e6c7d2
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-23 15:14:33 +0000

    update avatar border

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
-    const {user} = useContext(UserContext)
+    const { user } = useContext(UserContext)
     return (
         <div className="header-wrapper">
             <h1>NC News</h1>
-            <div className="login-wrapper">  
+            <div className="login-wrapper">
                 <Link id="link" to="/login">
                     <p id="link-login">log in</p>
                 </Link>
-                <img className="avatar" src={user.avatar_url}/>
+                {user.username && <img className="avatar" src={user.avatar_url} />}
                 <p className="header-username">{user.username}</p>
             </div>
         </div>
