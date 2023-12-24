c5aa974b2f83cb3972917ea6c18641deca340a76
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-24 18:53:13 +0000

    update styling to use grid and align columns

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -4,11 +4,11 @@ import "./ArticleCard.css"
 const ArticleCard = ({ article }) => {
     return (
         <li className="article-card">
-            <p className="article-votes">{article.votes} {article.votes === 1 ? "vote" : "votes"}</p>
+            <p className="article-votes">{article.votes}<br/> {article.votes === 1 ? "vote" : "votes"}</p>
             <img className="article-card-img" src={article.article_img_url}></img>
             <div className="article-wrapper">
                 <p className="article-date">{article.topic} | {convertDate(article.created_at)}</p>
-                <h2 className="article-card-title">{article.title} <span className="article-author">{article.author}</span></h2> 
+                <h2 className="article-card-title">{article.title} <span className="article-author">{article.author}</span></h2>
 
                 <p className="article-comment">{article.comment_count} {article.comment_count === 1 ? "comment" : "comments"}</p>
             </div>
